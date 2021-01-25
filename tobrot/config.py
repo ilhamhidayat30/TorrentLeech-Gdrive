523ceb711b92370bcf91e3b43f3e69c9e2514dec
ilhamhidayat30 <77956452+ilhamhidayat30@users.noreply.github.com>
2021-01-25 19:07:10 +0700

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -8,12 +8,12 @@ class Config(Config):
     API_HASH = "0d3e2f8dd300e43eeda1c071514193ee" #imp
     AUTH_CHANNEL = [-1001157751727, 755379549] #imp replace your_id with your id from telegram or delete
     INDEX_LINK = "https://drive.aang69.workers.dev/0:/"
-    GLEECH_COMMAND = "gleech@lipaickermannnbot"
-    YTDL_COMMAND = 'ytdl@lipaickermannnbot'
-    TELEGRAM_LEECH_COMMAND_G = "tleech@lipaickermannnbot"
+    GLEECH_COMMAND = "gleech@saytamaaabot"
+    YTDL_COMMAND = 'ytdl@saytamaaabot'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@saytamaaabot"
     CLONE_COMMAND_G = "gclone"
     PYTDL_COMMAND_G = "pytdl"
     DESTINATION_FOLDER = "Aang Drive"
-    LEECH_COMMAND = "leech@lipaickermannnbot"
-    CANCEL_COMMAND_G = "cancel@lipaickermannnbot"
+    LEECH_COMMAND = "leech@saytamaaabot"
+    CANCEL_COMMAND_G = "cancel@saytamaaabot"
     RCLONE_CONFIG = """"""
