263dee3847f5a6e4912bc3fa6d78da15093aad9b
ilhamhidayat30 <77956452+ilhamhidayat30@users.noreply.github.com>
2021-01-25 18:37:10 +0700

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,19 @@
+from tobrot.sample_config import Config
+
+#read readme too before filling these stuffs
+
+class Config(Config):
+    TG_BOT_TOKEN= "1524868195:AAEIsT5zVyxoDEQNY8x8ITTjpDox56CaiEo" #imp
+    APP_ID = 2601725 #imp
+    API_HASH = "0d3e2f8dd300e43eeda1c071514193ee" #imp
+    AUTH_CHANNEL = [-1001157751727, 755379549] #imp replace your_id with your id from telegram or delete
+    INDEX_LINK = "https://drive.aang69.workers.dev/0:/"
+    GLEECH_COMMAND = "gleech@lipaickermannnbot"
+    YTDL_COMMAND = 'ytdl@lipaickermannnbot'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@lipaickermannnbot"
+    CLONE_COMMAND_G = "gclone"
+    PYTDL_COMMAND_G = "pytdl"
+    DESTINATION_FOLDER = "Aang Drive"
+    LEECH_COMMAND = "leech@lipaickermannnbot"
+    CANCEL_COMMAND_G = "cancel@lipaickermannnbot"
+    RCLONE_CONFIG = """"""
